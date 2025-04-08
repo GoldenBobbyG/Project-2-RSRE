48c65f681960dcd4b84d2f096508ee5ddf128be4
Robb Strick <stricklandmba2011@gmail.com>
2025-04-08 15:10:43 -0400

    modified index src file

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -7,7 +7,9 @@ import { PartFactory } from './part.js';
 
 const sequelize = process.env.DB_URL
     ? new Sequelize(process.env.DB_URL) 
-    : new Sequelize(process.env.DB_Name || '', process.env.DB_PASSWORD, {
+    : new Sequelize(process.env.DB_NAME || '', 
+        process.env.DB_USER || '',
+        process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'postgres',
         dialectOptions: {
